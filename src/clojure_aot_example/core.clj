a435da35ec24992e3260812393bad47cf7049ffc
Alf Kristian Stoyle <alf.kristian@gmail.com>
2012-03-30 14:36:06 -0600

    Initial commit

---
+++ src/clojure_aot_example/core.clj
@@ -0,0 +1,1 @@
+(ns clojure-aot-example.core)
---
+++ test/clojure_aot_example/test/core.clj
@@ -0,0 +1,6 @@
+(ns clojure-aot-example.test.core
+  (:use [clojure-aot-example.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
