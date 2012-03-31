396ac7a5294f2872b55d7f8c15f10ab67ec5611b
Alf Kristian Stoyle <alf.kristian@gmail.com>
2012-03-31 09:47:21 -0600

    Better name of project.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojure-aot-example "1.0.0-SNAPSHOT"
+(defproject clojure-mutable-namespace-example "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.3.0"]]
   :aot  [clojure-aot-example.core]
