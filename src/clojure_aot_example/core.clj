105229b64c82b38e35945dbccc47fcddf9ac4bcc
Alf Kristian Stoyle <alf.kristian@gmail.com>
2012-03-30 18:25:44 -0600

    Various ways of changing a Var. Scary...

--- src/clojure_aot_example/core.clj
+++ src/clojure_aot_example/core.clj
@@ -1,5 +1,16 @@
 (ns clojure-aot-example.core
   (:gen-class))
 
+(def foo "foo")
+
+(def foo "bar")  
+
+(do
+  (def foo "baz")
+  (println "creating baz"))
+  
+
 (defn -main [& args]
-  (prn args))
+  (println foo)
+  (def foo (read-line))
+  (println foo))
